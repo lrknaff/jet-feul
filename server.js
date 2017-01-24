5715b3db77f9d7637018a39f60dfcb6011e09da7
lrknaff <lacey.knaff@gmail.com>
2017-01-24 15:50:58 -0700

    initial get response for /api/folders with mock data

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ const app = express();
 const path = require('path');
 
 app.set('port', process.env.PORT || 3000);
+app.locals.folders = {
+  folder1: "Lacey's Folder",
+  folder2: "Dale's Folder"
+}
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
-// app.get('/', (request, response) => {
-//   response.send('It\'s a secret to everyone.');
-// });
+app.get('/api/folders', (request, response) => {
+  response.json(app.locals.folders)
+})
 
 
 app.listen(app.get('port'), () => {
