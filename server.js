313b010a02e0803ff080e09958d90adb3df6b0c2
lrknaff <lacey.knaff@gmail.com>
2017-01-24 17:02:09 -0700

    initial post request to postman working

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const bodyParser = require('body-parser');
+const md5 = require('md5');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('port', process.env.PORT || 3000);
+
+app.locals.title = 'Jet Fuel'
 app.locals.folders = {
   folder1: "Lacey's Folder",
   folder2: "Dale's Folder"
@@ -12,6 +19,30 @@ app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.get('/api/folders', (request, response) => {
   response.json(app.locals.folders)
+})
+
+app.post('/api/folders', (request, response) => {
+  const { folder } = request.body
+  const id = Date.now()
+
+  if (!folder) {
+   return response.status(422).send({
+     error: 'No folder property provided'
+   });
+ }
+
+  app.locals.folders[id] = folder
+
+  response.status(201).json({ id, folder })
+});
+
+app.get('/api/folders/:id', (request, response) => {
+  const { id } = request.params;
+  const folder = app.locals.folders[id]
+
+  if(!folder) { return response.sendStatus(404); }
+
+  response.json({ id, folder })
 })
 
 
