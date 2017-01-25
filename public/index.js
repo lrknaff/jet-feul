5620c39d662e8f9fefd378364f7021cce45eeb59
lrknaff <lacey.knaff@gmail.com>
2017-01-24 19:27:08 -0700

    connect form to post on express route /api/folders

--- public/index.js
+++ public/index.js
@@ -5,6 +5,12 @@ $('.add-folder-button').on('click', function(e) {
 
   console.log($('.add-folder-input').val())
   var folder = $('.add-folder-input').val()
-
-  
+  $.ajax({
+    url: '/api/folders',
+    type: 'post',
+    data: {
+      folder: $('.add-folder-input').val(),
+      id: Date.now()
+    }
+  })
 })
