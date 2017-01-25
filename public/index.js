d87de172072d5d28d7fa1671407bca9190b58213
lrknaff <lacey.knaff@gmail.com>
2017-01-24 20:30:15 -0700

    display folders on refresh

--- public/index.js
+++ public/index.js
@@ -16,5 +16,12 @@ $('.add-folder-button').on('click', function(e) {
 
 function displayFolders(jsonData) {
   console.log(jsonData)
-  $folderSection.append(`${jsonData.folder}`)
+  $folderSection.append(`<div class="folder">+${jsonData.folder}</div>`)
 }
+
+$.get('/api/folders', function(data) {
+  for(var key in data) {
+    if (data.hasOwnProperty(key))
+    $folderSection.append(`<div class="folder">+${data[key].folder}</div>`)
+  }
+})
