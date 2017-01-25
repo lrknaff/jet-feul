6df20ee54cf1905cc3376d89f81f000f6adbe0f0
lrknaff <lacey.knaff@gmail.com>
2017-01-25 14:23:59 -0700

    working on displaying form of for link on click

--- public/index.js
+++ public/index.js
@@ -1,5 +1,16 @@
 var $folderSection = $('.folders')
 
+function displayFolders(jsonData) {
+  $folderSection.append(`<div class="folder" id=${jsonData.id}>+ ${jsonData.folder_name}</div><button type="submit" class="add-link-button">- add link</button>`)
+}
+
+$.get('/api/folders', function(data) {
+  for(var key in data) {
+    if (data.hasOwnProperty(key))
+    $folderSection.append(`<div class="folder" id=${data[key].id}>+ ${data[key].folder_name}</div><button type="submit" class="add-link-button">- add link</button>`)
+  }
+})
+
 $('.add-folder-button').on('click', function(e) {
   e.preventDefault()
   var folder = $('.add-folder-input').val()
@@ -12,17 +23,6 @@ $('.add-folder-button').on('click', function(e) {
     },
     success: displayFolders
   })
-})
-
-function displayFolders(jsonData) {
-  $folderSection.append(`<div class="folder" id=${jsonData.id}>+ ${jsonData.folder_name}</div><button type="submit">- add link</button>`)
-}
-
-$.get('/api/folders', function(data) {
-  for(var key in data) {
-    if (data.hasOwnProperty(key))
-    $folderSection.append(`<div class="folder" id=${data[key].id}>+ ${data[key].folder_name}</div><button type="submit">- add link</button>`)
-  }
 })
 
 $('.add-url-button').on('click', function(e) {
@@ -38,3 +38,12 @@ $('.add-url-button').on('click', function(e) {
     // success: displayUrl
   })
 })
+
+// $('.add-link-button').on('click', function(e) {
+//   e.preventDefault();
+//   $this.append(`<label action="/api/urls" method="post">
+//     Add Link:
+//     <input class="add-url-input" type="text" placeholder="Paste a link to shorten it"/>
+//     <input class="add-url-button" type="submit" value="Shorten link" />
+//   </label>`)
+// })
