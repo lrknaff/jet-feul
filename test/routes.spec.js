8c1e5ec11ef1bd31d54b18446ecef57eb9c8e444
lrknaff <lacey.knaff@gmail.com>
2017-01-26 21:48:05 -0700

    add basic test for all endpoints

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -17,13 +17,97 @@ describe('GET /api/folders', function() {
         done()
       })
   });
-  it('should return all folders', function(done) {
+  it('should return json', function(done) {
     chai.request(server)
     .get('/api/folders')
     .end(function(err, res) {
-    res.should.be.json; // jshint ignore:line
-    res.body.should.be.a('array');
-    res.body.should.have.property('Folder 1');
+      res.should.be.json; // jshint ignore:line
+      done()
+    })
+  });
+  it('should be an array', function(done) {
+    chai.request(server)
+    .get('/api/folders')
+    .end(function(err, res) {
+      res.body.should.be.a('array');
+      done()
+    })
+  })
+  it('should return folders', function(done) {
+    chai.request(server)
+    .get('/api/folders')
+    .end(function(err, res) {
+    res.body.should.have.property('folder_name');
+    done();
+    });
+  });
+});
+
+describe('GET /api/urls', function() {
+  it('should return a 200 status code', function(done) {
+      chai.request(server)
+      .get('/api/urls')
+      .end(function(err, res) {
+        res.should.have.status(200);
+        done()
+      })
+  });
+  it('should return json', function(done) {
+    chai.request(server)
+    .get('/api/urls')
+    .end(function(err, res) {
+      res.should.be.json; // jshint ignore:line
+      done()
+    })
+  });
+  it('should be an array', function(done) {
+    chai.request(server)
+    .get('/api/urls')
+    .end(function(err, res) {
+      res.body.should.be.a('array');
+      done()
+    })
+  })
+  it('should return urls', function(done) {
+    chai.request(server)
+    .get('/api/urls')
+    .end(function(err, res) {
+    res.body.should.have.property('id');
+    done();
+    });
+  });
+});
+
+describe('GET /api/urls/:id', function() {
+  it('should return a 200 status code', function(done) {
+      chai.request(server)
+      .get('/api/urls/27')
+      .end(function(err, res) {
+        res.should.have.status(200);
+        done()
+      })
+  });
+  it('should return json', function(done) {
+    chai.request(server)
+    .get('/api/urls/27')
+    .end(function(err, res) {
+      res.should.be.json; // jshint ignore:line
+      done()
+    })
+  });
+  it('should be an array', function(done) {
+    chai.request(server)
+    .get('/api/urls/27')
+    .end(function(err, res) {
+      res.body.should.be.a('array');
+      done()
+    })
+  })
+  it('should return one url', function(done) {
+    chai.request(server)
+    .get('/api/urls')
+    .end(function(err, res) {
+    res.body.should.have.property('id');
     done();
     });
   });
